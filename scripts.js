1f8aaa1105cc9864deaf7f6d2ea555bcdda89170
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2022-07-11 18:39:38 -0400

    Removed seconds from dispolay

--- scripts.js
+++ scripts.js
@@ -27,7 +27,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
 
     return hours + ":" + minutes;
   }
