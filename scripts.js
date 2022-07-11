c7d1301edb7e586ede700b1768d7d3d077a1381f
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2022-07-11 18:41:07 -0400

    Removed seconds from dispolay 2

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ window.addEventListener("DOMContentLoaded", () => {
     currentHours += 1
     currentMinutes -= 60
   }
-  endtime.setHours(currentHours, currentMinutes, currentSeconds)
+  endtime.setHours(currentHours, currentMinutes)
 
   function msToTime(duration) {
     let seconds = Math.floor((duration / 1000) % 60);
@@ -27,7 +27,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
-
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
 
     return hours + ":" + minutes;
   }
